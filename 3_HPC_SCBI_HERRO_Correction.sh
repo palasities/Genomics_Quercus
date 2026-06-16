a4a9ce49cb561dfdf5588cf29b00a32a39be4afe
Jorge Pallares <150237309+palasities@users.noreply.github.com>
2026-06-16 09:20:02 +0200

    Modify SLURM parameters and add GPU monitoring
    Updated SLURM job parameters for HERRO correction script, including job name, CPU count, memory, and time limits. Added GPU monitoring functionality and modified paths for input and output files.

--- 3_HPC_SCBI_HERRO_Correction.sh
+++ 3_HPC_SCBI_HERRO_Correction.sh
@@ -2,32 +2,38 @@
 # Leave only one comment symbol on selected options
 # Those with two commets will be ignored:
 # The name to show in queue lists for this job:
-#SBATCH -J 0_preprocess_HERRO_LIB009.sh
+#SBATCH -J Dorado_herro_inference_LIB009_Qilexilex_Norganelles.sh.sh
 
 # Number of desired cpus (can be in any node):
 #SBATCH --ntasks=1
 
 # Number of desired cpus (all in same node):
-#SBATCH --cpus-per-task=100
+#SBATCH --cpus-per-task=64
 
 # Amount of RAM needed for this job:
-#SBATCH --mem=400gb
+#SBATCH --mem=300gb
 
 # The time the job will be running:
-#SBATCH --time=68:00:00
+#SBATCH --time=80:00:00
 
-# To use GPUs you have to request them:
-##SBATCH --gres=gpu:1
+#########GPU#############
+#########################
+# * request GPU:
+
+# To use GPU, comment out the previous constraint line and uncomment these two following lines.
+#SBATCH --constraint=dgx
+#SBATCH --gres=gpu:1
 
 # If you need nodes with special features leave only one # in the desired SBATCH constraint line. cal is selected by default:
 # * to request any machine without GPU - DEFAULT
-#SBATCH --constraint=cal
+##SBATCH --constraint=cal
 # * to request only the machines with 128 cores and 1800GB of usable RAM
 ##SBATCH --constraint=bigmem
 # * to request only the machines with 128 cores and 450GB of usable RAM (
 ##SBATCH --constraint=sr
 # * to request only the machines with 52 cores and 187GB of usable RAM (
 ##SBATCH --constraint=sd
+
 
 # Set output and error files
 #SBATCH --error=job.%J.err
@@ -36,18 +42,53 @@
 # Leave one comment in following line to make an array job. Then N jobs will be launched. In each one SLURM_ARRAY_TASK_ID will take one value from 1 to 100
 ##SBATCH --array=1-100
 
-
 # To load some software (you can show the list with 'module avail'):
 
-module load seqkit ##needed
+#####################
+#####################
 
-# the program to execute with its parameters:
+lecturas_LIB009='/mnt2/fscratch/users/forescent_001_upm/jpallares/ICIFOR/Quercus_assemblies/2_remove_Organelles/LIB009/paf_methodology/Qilexilex.noOrganelle.fastq'
+modelo_celda='/mnt/home/users/forescent_001_upm/jpallares/HOME/modules/dorado_v.1.3.0/dorado-1.3.0-linux-x64/bin/herro-v1'
+lecturas_dorado_herro_corregidas='/mnt2/fscratch/users/forescent_001_upm/jpallares/ICIFOR/Quercus_assemblies/4_correction_HERRO/LIB009/directo_without_preprocess'
 
-##IMPORTANT TO ADD PORECHOP absolute path to preprocess script!
 
-program='/mnt/home/users/forescent_001_upm/jpallares/HOME/modules/HERRO_ONT_correction/herro/scripts'
-noOrganelles='/mnt/home/users/forescent_001_upm/jpallares/fscratch/ICIFOR/Quercus_assemblies/2_remove_Organelles/LIB009/paf_methodology/Qilexilex.noOrganelle.fastq'
+####un log para el uso de GPUs
+gpu_log="$lecturas_dorado_herro_corregidas/gpu_monitor_${SLURM_JOB_ID}.log"
 
-##scripts/preprocess.sh <input_fastq> <output_prefix> <number_of_threads> <parts_to_split_job_into>
+##carpeta de dorado
+cd /mnt/home/users/forescent_001_upm/jpallares/HOME/modules/dorado_v.1.3.0/dorado-1.3.0-linux-x64/bin
 
-$program/preprocess.sh $noOrganelles /mnt/home/users/forescent_001_upm/jpallares/fscratch/ICIFOR/Quercus_assemblies/4_correction_HERRO/LIB009/1_preprocess/Qilexilex 100 5
+
+echo "HOSTNAME: $(hostname)"
+echo "CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-not_set}"
+
+(
+  while true; do
+    echo "===== $(date '+%F %T') =====" >> "$gpu_log"
+    nvidia-smi \
+      --query-gpu=index,name,utilization.gpu,utilization.memory,memory.total,memory.used,memory.free,temperature.gpu,power.draw \
+      --format=csv,noheader,nounits >> "$gpu_log"
+    echo "--- processes ---" >> "$gpu_log"
+    nvidia-smi \
+      --query-compute-apps=gpu_uuid,pid,process_name,used_memory \
+      --format=csv,noheader,nounits >> "$gpu_log" 2>/dev/null || true
+    echo >> "$gpu_log"
+    sleep 600
+  done
+) &
+MONITOR_PID=$!
+
+trap 'kill $MONITOR_PID 2>/dev/null || true' EXIT
+
+export LD_LIBRARY_PATH=/mnt/home/users/forescent_001_upm/jpallares/HOME/modules/dorado_v.1.3.0/dorado-1.3.0-linux-x64/lib
+
+./dorado correct $lecturas_LIB009 --device cuda:0 --threads 64 --infer-threads 3 --model-path $modelo_celda > $lecturas_dorado_herro_corregidas/Qilexilex.LIB009.noOrganelle.herro.fasta
+
+
+
+DORADO_EXIT=$?
+
+kill $MONITOR_PID 2>/dev/null || true
+wait $MONITOR_PID 2>/dev/null || true
+
+exit $DORADO_EXIT
